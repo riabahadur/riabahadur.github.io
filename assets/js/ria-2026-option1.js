61ed6c9544a267ee08e050e987ebd0132beee2d1
riabahadur <ria.bahadur@gmail.com>
2026-08-21 13:22:32 -0700

    Apply first-pass site review refinements

--- assets/js/ria-2026-option1.js
+++
@@ -1,1 +0,0 @@
-const menu=document.querySelector('.menu'); const mobile=document.querySelector('.mobile'); if(menu&&mobile){menu.addEventListener('click',()=>{mobile.classList.toggle('open');menu.setAttribute('aria-expanded',mobile.classList.contains('open'));});} document.querySelectorAll('.mobile a').forEach(a=>a.addEventListener('click',()=>mobile?.classList.remove('open')));
